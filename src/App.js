20070cf10ecb5086dcbc30e20a531adfe6d1f473
KamilaMania <mania.kamika@icloud.com>
2020-05-25 13:32:12 +0200

    structure of a page and tourlist component added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import "./App.scss";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Navbar from "./components/Navbar/Navbar.js";
+import TourList from "./components/TourList";
+
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Navbar />
+        <TourList />
       </React.Fragment>
     );
   }
---
+++ src/components/Tour/Tour.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import "./tour.scss";
+export default class Tour extends Component {
+  render() {
+    return <h1>TOUR</h1>;
+  }
+}
