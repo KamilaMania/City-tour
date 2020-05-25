ea70612d5f0f39948ff3e39cbc30c2e1d163fa61
KamilaMania <mania.kamika@icloud.com>
2020-05-25 13:03:25 +0200

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import "./App.scss";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import Navbar from "./components/Navbar/Navbar.js";
 class App extends Component {
   render() {
-    return <h1>Tour App</h1>;
+    return (
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
+    );
   }
 }
 
